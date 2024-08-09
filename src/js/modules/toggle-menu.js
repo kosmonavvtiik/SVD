ef6da5f964f3d3feff23cbe1519bd7f0c724221f
kosmonavvtiik <kosmonavvtik@mail.ru>
2024-08-10 00:55:14 +0300

    изменение header

--- src/js/modules/toggle-menu.js
+++ src/js/modules/toggle-menu.js
@@ -1,16 +1,22 @@
 const menuInit = () => {
-    const toggleMenu = document.querySelector('.toggle-menu')
+    const toggleMenu = document.querySelector ('.toggle-menu')
 
-    if (toggleMenu) return
-    
-    const menu = document.querySelector ('.nav__list')
+    if (!toggleMenu) return
+
+    const menu = document.querySelector ('.nav__menu')
+    const navSocial = document.querySelector('.nav__social')
+    const requisites = document.querySelector('.requisites')
+    const politics = document.querySelector('.politics')
 
     const handleToggleMenu = () => {
-        toggleMenu.classlist.toggle('toggle-menu_active')
-        menu.classList.toggle('list_active')
+        toggleMenu.classList.toggle('toggle-menu_active')
+        menu.classList.toggle('nav__menu_active')
+        navSocial.classList.toggle('nav__social_active')
+        requisites.classList.toggle('requisites_active')
+        politics.classList.toggle('politics_active')
     }
-    
+
     toggleMenu.addEventListener('click', handleToggleMenu)
 }
 
-export default menuInit()
+export default menuInit
